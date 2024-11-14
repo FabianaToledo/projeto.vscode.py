1af268ba0f3a361a72b8fbd132270a1bf04c41cd
FabianaToledo <fabianatoledo.al@gmail.com>
2024-11-14 16:03:01 -0300

    teste 1 - commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("jornada de dados ++")
+
+# variáveis
+idade = 34
+nome = "Fabian"
+
+#print
+print(idade)
+
+#Soma
+nova_idade = idade + 1 
+print(nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#métodos
+lista_invertida = lista.reverse()
+print(lista)
+#for i in range(4) :
+    #print(i)
+ #   print(lista_invertida[i])
